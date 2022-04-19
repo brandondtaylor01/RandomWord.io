3f13e6341c88836b5ba76ccb7e81d46d6ee7c7a7
Brandon Taylor <brandondtaylor01@gmail.com>
2022-04-19 18:37:09 -0400

    Added throttling mechanic, fixed favicons

--- client/src/Components/WordContainer/index.jsx
+++ client/src/Components/WordContainer/index.jsx
@@ -1,22 +1,63 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Box, ButtonGroup, Button } from "@mui/material";
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import Word from "../Word";
 
 export default function WordContainer() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState();
+  const [locked, setLocked] = useState(false);
 
-  function handleShuffle() {
-    fetch( 'https://www.randomword.io:7777/get-word').then(res => res.json()).then(res => {
-      if(res?.success) {
-        if(typeof res.word !== 'undefined') {
-          setWord(res.word);
-        }
-      }
+  function fetchWord() {
+    return new Promise((resolve, reject) => {
+      // fetch the next word.
+      fetch( 'https://www.randomword.io:7777/get-word').then(res => res.json()).then(res => {
+
+        // if we were successful, resolve the Promise, else, reject it.
+        return (res?.success) ? resolve(res.word) : reject(new Error(res))
+      }).catch(err => {
+        console.log(err);
+        return reject(new Error({
+          success: false,
+          error: err
+        }));
+      });
+    });
+  }
+
+
+  const getWord = useCallback(() => {
+
+    setLocked(true);
+
+    // fetch the word and update the state.
+    fetchWord().then(res => {
+      setWord(res);
+
+      // set lock timeout.
+      setTimeout(() => {
+        setLocked(false);
+      }, 1500);
     }).catch(err => {
       console.log(err);
-    })
+    });
+  }, [])
+
+  function handleShuffle() {
+
+    // if we aren't locked.
+    // we do this because we don't want the user requesting in rapid succession.
+    if(!locked) {
+      getWord();
+    }
   }
+
+  // Lifecycle
+  // Run-once to setup the initial word.
+  useEffect(() => {
+    if(word === '' || typeof word === 'undefined') {
+      getWord();
+    }
+  }, [word, getWord]);
 
   return(
     <Box
@@ -39,7 +80,7 @@ export default function WordContainer() {
       </Box>
       <Box sx={{marginTop: '16px'}}>
         <ButtonGroup variant="contained">
-          <Button onClick={handleShuffle}>
+          <Button onClick={handleShuffle} disabled={locked}>
               <ShuffleIcon fontSize='large' />
             </Button>
         </ButtonGroup>
